edb1aed362f6bc65f4441ce9e096932798245e42
ilya <ilyaozerov.gg@gmail.com>
2022-04-05 20:40:40 +0300

    Replaced required quantity from 1 to 0

--- src/redux/productsSelectors.ts
+++ src/redux/productsSelectors.ts
@@ -5,7 +5,7 @@ export const selectProducts = (state: AppStateType) => {
     // if filter options enabled
     // return filtered products
     if (state.productsPage.filter?.isAvailable) {
-        return state.productsPage.products.filter(product => product.quantity > 1);
+        return state.productsPage.products.filter(product => product.quantity > 0);
     }
 
     return state.productsPage.products;
