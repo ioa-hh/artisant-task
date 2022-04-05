9006477c896b95c55482922975dc85c0b20bf90d
ilya <ilyaozerov.gg@gmail.com>
2022-04-05 20:19:09 +0300

    Added App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react';
+import "./App.scss"
+import {Header} from './components/Header/Header';
+import {Gallery} from "./components/Gallery/Gallery";
+import {Provider, useDispatch, useSelector} from "react-redux";
+import store from './redux/store';
+import {requestProducts} from "./redux/productsReducer";
+import {selectProducts} from "./redux/productsSelectors";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App: React.FC = () => {
+
+    const dispatch = useDispatch();
+    const products = useSelector(selectProducts);
+
+    // request products when app started
+    useEffect(() => {
+        dispatch(requestProducts());
+    }, []);
+
+    return (
+        <div className="wrapper">
+            <Header/>
+            <Gallery products={products}/>
+        </div>
+    );
 }
 
-export default App;
+export const ArtisantTaskApp: React.FC = () => {
+    return (
+        <Provider store={store}>
+            <App />
+        </Provider>
+    );
+}
